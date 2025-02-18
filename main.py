870ecf1d13f23997df297ddfd2df2e10233ebd39
justinwilliams <justintw51@gmail.com>
2025-02-17 20:57:18 -0500

    Fixed the issue with pushing now working on code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-jreaknlvjrnvlcjn
+import time
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    print(x)
+    time.sleep(1)
+
+print("TIME'S UP")
